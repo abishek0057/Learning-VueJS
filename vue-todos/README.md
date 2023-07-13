0ec95d6527a8ac56045bdc4509bdb52acf72b140
abishek0057 <abishekparajuli64@gmail.com>
2023-07-13 11:23:12 +0545

    rearrange folder

---
+++ vue-todos/README.md
@@ -0,0 +1,1 @@
+[Tutorial Link](https://www.youtube.com/watch?v=KTFH4P8unUQ&list=LL&index=3&ab_channel=JohnKomarnicki)
---
+++ vue-todos/src/views/AboutView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="about">
+    <h1>About the Todo List App</h1>
+    <p>
+      Welcome to the Todo List App! We are delighted to introduce you to a
+      powerful task management tool designed to enhance your productivity and
+      simplify your daily life. With this app, managing your to-do lists has
+      never been easier.
+    </p>
+    <p>
+      Our mission is to provide you with a seamless and intuitive user
+      experience, allowing you to stay organized, focused, and in control of
+      your tasks. We understand the challenges of a busy schedule, juggling
+      multiple responsibilities, and trying to remember every detail. That's why
+      we built this app – to help you stay on top of your to-do list
+      effortlessly.
+    </p>
+    <h3>Key Features:</h3>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, architecto
+      corrupti qui officia modi sunt reiciendis asperiores pariatur, assumenda
+      voluptas distinctio sapiente aut itaque, tempore molestiae nostrum dolorum
+      laborum labore! Id dolore error culpa quidem voluptates ipsam aliquam sed,
+      doloribus possimus, distinctio expedita vero iste fugiat ratione nesciunt
+      labore non. Ipsam amet, quod aperiam obcaecati consequatur quo nesciunt
+      explicabo repellat. Nemo illo sed omnis nisi pariatur eaque nihil aliquid
+      facilis necessitatibus soluta quam minus repellat, quia quisquam cum
+      similique consectetur nobis, repellendus vitae fugit esse maxime! Nobis
+      ipsum at pariatur! Dolorum atque est impedit nihil ut labore temporibus?
+      Non, in temporibus illum vitae consequatur eaque fugiat accusantium cum
+      beatae, laborum id, quidem voluptatibus! Tenetur, corporis. Magnam
+      voluptatem neque corporis unde? Quia necessitatibus voluptate minus
+      corporis ut placeat dolores illum iusto fuga expedita itaque atque nam,
+      pariatur voluptates dolore, doloremque quis aut ipsa? Sequi eos debitis
+      corporis veniam! Accusamus, nobis quis. Deleniti minus, ipsam ducimus,
+      vitae recusandae harum id numquam assumenda debitis mollitia hic rem
+      explicabo ab aut neque quaerat, nihil alias dolores fugit voluptate
+      officiis illum veniam. Recusandae, accusamus assumenda. Distinctio, et
+      accusamus illum iste voluptate quos. Ipsam autem voluptas nihil, expedita
+      ullam nisi enim quas sint tempora accusamus laboriosam tenetur eligendi
+      officia modi facere dolorum veniam porro ratione odio. Unde accusamus
+      assumenda consequatur culpa minima esse doloremque laudantium, beatae
+      fugit repellendus modi iusto optio id odio explicabo minus ipsa, sint
+      quaerat rem harum, asperiores delectus doloribus recusandae voluptatem.
+      Magnam? Neque iusto repellendus consectetur? Sit atque neque, fugit, iste
+      eius recusandae perspiciatis iusto quas eligendi qui, eum dolores maxime
+      quaerat provident. Et corrupti quaerat quia consectetur magni saepe
+      doloremque laudantium. Harum maiores voluptatibus iste mollitia quidem
+      voluptatem, nostrum commodi eveniet, dolorem, sunt reprehenderit.
+      Reprehenderit consequuntur totam labore beatae illo magni quo pariatur,
+      veniam necessitatibus voluptate fugiat exercitationem. In, consectetur
+      dignissimos?
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+@media (min-width: 1024px) {
+  .about {
+    min-height: 80vh;
+    max-width: 1100px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    p {
+      text-align: justify;
+    }
+    h1 {
+      text-align: center;
+      margin: 15px 0;
+    }
+    h3 {
+      margin-top: 15px;
+    }
+  }
+}
+</style>
+
